00640aadffbe65bdbe6035b864ca014ce31a2b78
nguyenvanduocit+ <nguyenvanduocit@gmail.com>
2022-09-07 04:46:51 +0700

    inti

--- io.go
+++ io.go
@@ -81,3 +81,29 @@ func ReadRequest(r *http.Request, message interface{}) error {
 
 	return nil
 }
+func ProtobufHandler(w http.ResponseWriter, r *http.Request, request proto.Message, do func(proto.Message) (proto.Message, error)) {
+	if err := ApplyCors(w, r); err != nil {
+		WriteError(w, r, http.StatusInternalServerError, err)
+		return
+	}
+
+	if err := ApplyContentType(w, r); err != nil {
+		WriteError(w, r, http.StatusInternalServerError, err)
+		return
+	}
+
+	if err := ReadRequest(r, request); err != nil {
+		WriteError(w, r, http.StatusBadRequest, err)
+		return
+	}
+
+	response, err := do(request)
+	if err != nil {
+		WriteError(w, r, http.StatusInternalServerError, err)
+		return
+	}
+
+	if err := WriteResponse(w, r, response); err != nil {
+		WriteError(w, r, http.StatusInternalServerError, err)
+	}
+}
